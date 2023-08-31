b77e4de6210f22f5eff5ca3214ea6c992e4e1752
Ashis443hkumar <112205180+Ashis443hkumar@users.noreply.github.com>
2023-08-31 20:56:28 +0530

    3 files modified

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -5,21 +5,21 @@ import {HeaderData} from "./dataStore"
 export default function Header() {
   return (
     <>
-    <section>
+    <section className="sidebar_menu">
       <div className="user_info">
         <div className="info_img">
-          <img src="" alt="" />
+          <i class="fa-solid fa-user"></i>
         </div>
         <span className="info_name">Ashish Sharma</span>
       </div>
       <nav className="navigation">
-        <ul classname="nav_list">
+        <ul className="nav_list">
         {
           HeaderData.map((value, id) =>(
             <li className="nav_items" key={value.id}>
               <a href="" className="nav_link">
-                <img src="" alt="" className="nav-link-icons" />
-                <span classname="nav_link_text" >{value.navMenu}</span>
+               <i class="fa-solid fa-house nav-link-icons"></i>
+                <span className="nav_link_text" >{value.navMenu}</span>
               </a>
             </li>
           ))
